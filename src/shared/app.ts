e84d9857b5baebe5482d65331feaa676475f1571
paulohenriquepm <paulo.henriquepm@outlook.com>
2021-10-07 22:17:56 -0300

    feat: add jest to tests

---
+++ src/shared/app.ts
@@ -0,0 +1,36 @@
+import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+
+import { AppError } from '@shared/errors/AppError';
+
+import { routes } from './routes';
+
+import './container';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  },
+);
+
+export { app };
+
+app.listen(3333, () => {
+  console.log('🚀 Server is running');
+});
--- src/shared/server.ts
+++ src/shared/server.ts
@@ -1,18 +1,5 @@
-import 'reflect-metadata';
-import express from 'express';
-import cors from 'cors';
-
-import { routes } from './routes';
-
-import './container';
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use(routes);
+import { app } from './app';
 
 app.listen(3333, () => {
-  console.log('🚀 Server is running');
+  console.log('Server is running 🚀');
 });
