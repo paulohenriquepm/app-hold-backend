9a0f6de08c7813a85e694eea6d2951d4593da077
paulohenriquepm <paulo.henriquepm@outlook.com>
2021-10-27 20:52:38 -0300

    feat: finish createUserController spec

--- spec/modules/users/useCases/createUser/createUserController.spec.ts
+++ spec/modules/users/useCases/createUser/createUserController.spec.ts
@@ -6,27 +6,59 @@ import { mockRequest } from '@utils/mockRequest';
 import { CreateUserUseCase } from '@modules/users/useCases/createUser/createUserUseCase';
 import { CreateUserController } from '@modules/users/useCases/createUser/createUserController';
 import { ICreateUserDTO } from '@modules/users/dtos/ICreateUserDTO';
+import { AppError } from '@shared/errors/AppError';
 
 jest.mock('@modules/users/useCases/createUser/createUserUseCase');
 
 const CreateUserUseCaseMock = CreateUserUseCase as jest.Mock<CreateUserUseCase>;
 const createUserUseCaseMock =
   new CreateUserUseCaseMock() as jest.Mocked<CreateUserUseCase>;
-
 const createUserController = new CreateUserController(createUserUseCaseMock);
 
-const request = mockRequest({
-  name: 'Foo Bar',
-  email: 'foo@bar.com',
-  password: 'foobar123',
-} as ICreateUserDTO);
 const response = mockResponse();
 
 describe('createUserController', () => {
-  it('test', async () => {
-    await createUserController.handle(request as Request, response as Response);
+  describe('when params are valid', () => {
+    const data = {
+      name: 'Foo Bar',
+      email: 'foo@bar.com',
+      password: 'foobar123',
+    } as ICreateUserDTO;
+    const request = mockRequest(data);
 
-    expect(response.status).toHaveBeenCalledWith(201);
-    expect(createUserUseCaseMock.execute).toHaveBeenCalled();
+    it('should return status 201 and call createUserUseCase with right params', async () => {
+      await createUserController.handle(
+        request as Request,
+        response as Response,
+      );
+
+      expect(response.status).toHaveBeenCalledWith(201);
+      expect(createUserUseCaseMock.execute).toHaveBeenCalledWith(data);
+    });
+  });
+
+  describe('when email already exists', () => {
+    const data = {} as ICreateUserDTO;
+    const request = mockRequest(data);
+
+    it('should return status with 400 and call createUserUseCase with right params', async () => {
+      let error: AppError;
+      createUserUseCaseMock.execute.mockRejectedValueOnce(
+        new AppError('E-mail já está em uso'),
+      );
+
+      try {
+        await createUserController.handle(
+          request as Request,
+          response as Response,
+        );
+      } catch (e) {
+        error = e;
+      }
+
+      expect(createUserUseCaseMock.execute).toHaveBeenCalledWith(data);
+      expect(error.message).toBe('E-mail já está em uso');
+      expect(error.statusCode).toBe(400);
+    });
   });
 });
