df22688804843dc1692aab668f64e04bbfde9670
paulohenriquepm <paulo.henriquepm@outlook.com>
2021-11-22 22:51:24 -0300

    fix: yahooApi controller dependencies

--- src/modules/yahooApi/useCases/importDataUseCase/index.ts
+++ src/modules/yahooApi/useCases/importDataUseCase/index.ts
@@ -1,5 +1,6 @@
 import { AssetsRepository } from '@modules/assets/repositories/implementations/assetsRepository';
 import { CreateAssetUseCase } from '@modules/assets/useCases/createAsset/createAssetUseCase';
+import { UpdateAssetUseCase } from '@modules/assets/useCases/updateAsset/updateAssetUseCase';
 import { YahooApi } from '@modules/yahooApi/api/yahooApi';
 import { companiesToImport } from '@modules/yahooApi/utils/companiesToImport';
 
@@ -8,12 +9,15 @@ import { YahooApiImportController } from './yahooApiImportController';
 
 const assetsRepository = new AssetsRepository();
 const createAssetUseCase = new CreateAssetUseCase(assetsRepository);
+const updateAssetUseCase = new UpdateAssetUseCase(assetsRepository);
 const yahooApi = new YahooApi();
 
 const importDataUseCase = new ImportDataUseCase(
   companiesToImport,
-  createAssetUseCase,
   yahooApi,
+  assetsRepository,
+  updateAssetUseCase,
+  createAssetUseCase,
 );
 
 const yahooApiImportController = new YahooApiImportController(
