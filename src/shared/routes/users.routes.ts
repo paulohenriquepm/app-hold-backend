8a32e98eb92dba274335462c748787155e7b04b3
paulohenriquepm <paulo.henriquepm@outlook.com>
2022-02-09 21:58:35 -0300

    fix: revert emails route

--- src/shared/routes/users.routes.ts
+++ src/shared/routes/users.routes.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 
 import { updateUserController } from '@modules/users/useCases/updateUser';
 import { deleteUserController } from '@modules/users/useCases/deleteUser';
-import prisma from '@shared/db/prisma';
 
 const usersRouter = Router();
 
@@ -11,15 +10,6 @@ usersRouter.put('/:id', (request, response) => {
 });
 usersRouter.delete('/:id', (request, response) => {
   return deleteUserController.handle(request, response);
-});
-usersRouter.get('/list/users-email', async (request, response) => {
-  const emails = await prisma.user.findMany({
-    select: {
-      email: true,
-    },
-  });
-
-  return response.json(emails);
 });
 
 export { usersRouter };
